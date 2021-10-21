d3d7269ad8d6ec224f539649fd0edaf2b46839e6
ckgod <rhckdrnr123@gmail.com>
2021-10-21 14:30:34 +0900

    queue implement

---
+++ stack/queue_impl.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+struct Queue {
+    int a[MAX_N];
+    int f, b;
+
+    void clear() { f = b = 0; }
+    void push(int x) { a[b++] = x; }
+    void pop() { f++; }
+    int front() { return a[f]; }
+    int back() { return a[b - 1]; }
+    int size() { return b - f; }
+};
+
+int main() {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+
+
+    return 0;
+}
